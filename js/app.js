b513ab70c66db04720b74cd332c7c9df792558ac
lexborelli <99988113+lexborelli@users.noreply.github.com>
2023-07-14 15:14:00 -0400

    trying to make arrow button work

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let employees = []; 
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`;
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US`;
 const gridContainer = document.querySelector(".grid-container"); 
 const overlay = document.querySelector(".overlay"); 
 const modalContainer = document.querySelector(".modal-content"); 
